cf0cef56724a9a147abf3316542468a96def6337
sakirhakimov <sfkh_0987>
2021-10-21 00:38:23 +0300

    master поправил конфиг

--- config/env.ts
+++ config/env.ts
@@ -2,3 +2,4 @@ export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || '123'
 export const POSTGRES_DB = process.env.POSTGRES_DB || 'foodapp'
 export const HOST = process.env.HOST || 'localhost'
 export const DB_PORT = process.env.DB_PORT ? +process.env.DB_PORT : 5432
+export const POSTGRES_USER = process.env.POSTGRES_USER || 'foodappadmin'
--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,11 @@
 import { ConnectionOptions } from 'typeorm'
-import { DB_PORT, HOST, POSTGRES_DB, POSTGRES_PASSWORD } from 'config/env'
+import { DB_PORT, HOST, POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER } from 'config/env'
 
 const config: ConnectionOptions = {
     type: 'postgres',
     host: HOST,
     port: DB_PORT,
-    username: 'postgres',
+    username: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
     database: POSTGRES_DB,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
